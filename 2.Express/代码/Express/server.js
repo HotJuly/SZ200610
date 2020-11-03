014fa8ba20d338e520cbbd970ef5a154ef11f32e
HotJuly <949588380@qq.com>
2020-11-03 14:33:32 +0800

    6.中间件使用

--- 2.Express/代码/Express/server.js
+++ 2.Express/代码/Express/server.js
@@ -1,111 +1,73 @@
+const { resolve } = require('path');
 const express = require('express');
+
+const multipart = require('connect-multiparty');
 
 // 1.创建服务器应用实例
 const app = express();  //  app ->  application
 
-
 /*
-    3.创建接口(路由,API)
-    接口:
-        1.请求方式
-        2.请求路径
-    接口文档:
-        method  GET 
-        path    /test
-    回调函数接收两个参数:
-        1.request   ->  请求报文对象(存放着所有跟请求相关的数据)
-        2.response  ->  响应报文对象(存放着所有跟响应相关的数据)
-            response的常用API
-                1.response.end(数据)    ->  将原数据返回给前端
-                2.response.send(数据)   ->  根据当前返回的数据类型,自动设置响应头的文件类型content-type
-                3.response.json(数据)   ->  自动设置响应头的文件类型为json,并返回数据
-                4.response.redirect()    ->  当用户请求该路由时,重定向到指定路由
-                5.response.set()    ->  用于配置响应报文对象
-                6.response.status()     ->用于指定当前响应的状态码,注意:设置完之后还需要返回响应
-                7.response.cookie()     ->  用于设置cookie
-    注意:
-        1.每个路由路径都可以被不同的请求方式重复注册
-        2.浏览器地址栏只能发送GET请求
-        3.想要成功请求一个接口,请求方式和请求路径都必须匹配(精准匹配)
-        4.可以通过response.set设置响应头,告知浏览器编码格式
-        5.路由地址必须是绝对路径
+    中间件
+    数据类型:函数
+    使用语法:
+        全局使用:app.use(中间件函数)
+        局部使用:app.post('/test',multipart(),路由中间件函数)
+    作用:
+        1.扩展一些功能
+        2.设置修改请求头和响应头
+        3.执行下一个中间件
+    传入实参:
+        1.req   ->  request
+        2.res   ->  response
+        3.next  ->  放行,执行下一个中间件
+    特点:
+        1.无论任何请求方式或者任何地址,都会触发中间件
+        2.路由其实也是中间件
+        3.每个中间件都可以去返回数据,但是比较靠前的中间件尽量不要返回数据,否则容易出现报错
+        4.无论是路由还是中间件,谁在前面就先执行谁
+        5.可以进行链式调用
 
-    面试题:什么是路由
-        1.路由可以理解为是一个键值对key-value
-        2.key->请求路径
-        3.value->返回的数据
-
-    面试题:路由的分类
-        1.前端路由
-            1)前端路由的解析是有对应的js库进行解析(Vue-Router,React-Router-DOM)
-            2)前端路由不走网络传输层(通过监视路径的变化,来显示不同的组件)
-            3)前端路由路径匹配时,返回的是"组件"
-
-            总结:前端路由就是路由路径和路由组件的映射关系
-
-            routes:[
-                {
-                    path:"/home",
-                    component:Home
-                }
-            ]
-
-        2.后端路由
-            1)后端路由的解析是由服务器进行解析
-            2)后端路由需要前端发送请求触发
-            3)后端路由路径匹配时,返回的一般是数据
-
-            总结:后端路由就是路由路径,请求方式与回调函数的映射关系
-
-    路由传参:
-            1.GET
-                1)query     /test?key1=value1
-                2)params    /test/:id   ->  /test/1   ->占位符需要在注册路由的时候提前声明好
-            2.POST
-                1)query     /test?key1=value1
-                  接收方式:req.query
-
-                2)params    /test/:id   ->  /test/1   ->占位符需要在注册路由的时候提前声明好
-                  接收方式:req.params
-
-                3)body(请求体)
-                    优点:
-                        1.相对query和params,body是以密文进行传递数据(安全性好)
-                        2.url是有长度限制的 ->  数据量不够大
-                            body传参没有长度先知->数据量较大
-                  接收方式:req.body
-                  原生接收方式:
-                    request.on('data',function(data){
-                            console.log('body',data.toString())
-                        })
+    分类:
+        1.内置中间件
+            express.static(绝对路径(需要通过path.resolve进行拼接))    ->  静态资源映射
+            express.json()  ->  application/json    ->  raw
+            express.urlencoded()    ->  x-www-form-urlencoded 
+        2.第三方中间件
+            connect-multiparty  ->  form-data
+        3.自定义中间件
+        4.路由中间件
 
 */
-app.get('/test',function(request,response){
-    console.log("query",request.query);
-    console.log("params",request.params);
 
-    console.log('/test get success')
+// app.use(function(req,res,next){
+//     console.log('中间件1号准备就绪');
+//     res.send('234');
+//     next();
+// })
 
-    //response.set()用于设置响应头配置
-    // response.set('content-type','text/html;charset=utf-8');
-    //response.end(需要返回的数据)
-    // response.end('haha哈哈')
-    // response.send('haha哈哈')
-    response.status(303).json('haha哈哈')
+// app.use(function(req,res,next){
+//     console.log('中间件1号弹射起飞,中间件2号准备就绪');
+//     next();
+// })
+
+app.use(express.json())
+.use(express.urlencoded())
+.use(multipart());
+// app.use(express.static(resolve(__dirname,"./public")))
+
+
+
+app.get('/test',function(request,response,next){
+    response.send('/test get success');
 })
 
-app.post('/test/:id',function(request,response){
-    console.log("query",request.query);
-    console.log("params",request.params);
-    request.on('data',function(data){
-        console.log('body',data.toString())
-    })
-    // console.log('body',request.body)
-    console.log('/test post success')
-
-    //response.end(需要返回的数据)
-    response.end('xixi')
+app.post('/test',multipart(),function(req,res,next){
+    // console.log('body-json',req.body)
+    // console.log('body-urlencoded',req.body)
+    console.log('body-formdata',req.body)
+    res.send('/test post success');
 })
+
 
 //2.将服务器应用实例挂载到某个端口上,并监听
 app.listen(3000,function(error){
---
+++ 2.Express/代码/Express/上午内容/server.js
@@ -0,0 +1,118 @@
+const { response } = require('express');
+const express = require('express');
+
+// 1.创建服务器应用实例
+const app = express();  //  app ->  application
+
+/*
+    3.创建接口(路由,API)
+    接口:
+        1.请求方式
+        2.请求路径
+    接口文档:
+        method  GET 
+        path    /test
+    回调函数接收两个参数:
+        1.request   ->  请求报文对象(存放着所有跟请求相关的数据)
+        2.response  ->  响应报文对象(存放着所有跟响应相关的数据)
+            response的常用API
+                1.response.end(数据)    ->  将原数据返回给前端
+                2.response.send(数据)   ->  根据当前返回的数据类型,自动设置响应头的文件类型content-type
+                3.response.json(数据)   ->  自动设置响应头的文件类型为json,并返回数据
+                4.response.redirect()    ->  当用户请求该路由时,重定向到指定路由
+                5.response.set()    ->  用于配置响应报文对象
+                6.response.status()     ->用于指定当前响应的状态码,注意:设置完之后还需要返回响应
+                7.response.cookie()     ->  用于设置cookie
+    注意:
+        1.每个路由路径都可以被不同的请求方式重复注册
+        2.浏览器地址栏只能发送GET请求
+        3.想要成功请求一个接口,请求方式和请求路径都必须匹配(精准匹配)
+        4.可以通过response.set设置响应头,告知浏览器编码格式
+        5.路由地址必须是绝对路径
+
+    面试题:什么是路由
+        1.路由可以理解为是一个键值对key-value
+        2.key->请求路径
+        3.value->返回的数据
+
+    面试题:路由的分类
+        1.前端路由
+            1)前端路由的解析是有对应的js库进行解析(Vue-Router,React-Router-DOM)
+            2)前端路由不走网络传输层(通过监视路径的变化,来显示不同的组件)
+            3)前端路由路径匹配时,返回的是"组件"
+
+            总结:前端路由就是路由路径和路由组件的映射关系
+
+            routes:[
+                {
+                    path:"/home",
+                    component:Home
+                }
+            ]
+
+        2.后端路由
+            1)后端路由的解析是由服务器进行解析
+            2)后端路由需要前端发送请求触发
+            3)后端路由路径匹配时,返回的一般是数据
+
+            总结:后端路由就是路由路径,请求方式与回调函数的映射关系
+
+    路由传参:
+            1.GET
+                1)query     /test?key1=value1
+                2)params    /test/:id   ->  /test/1   ->占位符需要在注册路由的时候提前声明好
+            2.POST
+                1)query     /test?key1=value1
+                  接收方式:req.query
+
+                2)params    /test/:id   ->  /test/1   ->占位符需要在注册路由的时候提前声明好
+                  接收方式:req.params
+
+                3)body(请求体)
+                    优点:
+                        1.相对query和params,body是以密文进行传递数据(安全性好)
+                        2.url是有长度限制的 ->  数据量不够大
+                            body传参没有长度先知->数据量较大
+                  接收方式:req.body
+                  原生接收方式:
+                    request.on('data',function(data){
+                            console.log('body',data.toString())
+                        })
+*/
+
+
+app.get('/test',function(request,response){
+    console.log("query",request.query);
+    console.log("params",request.params);
+
+    console.log('/test get success')
+
+    //response.set()用于设置响应头配置
+    // response.set('content-type','text/html;charset=utf-8');
+    //response.end(需要返回的数据)
+    // response.end('haha哈哈')
+    // response.send('haha哈哈')
+    response.status(200).json('haha哈哈')
+})
+
+app.post('/test/:id',function(request,response){
+    console.log("query",request.query);
+    console.log("params",request.params);
+    request.on('data',function(data){
+        console.log('body',data.toString())
+    })
+    // console.log('body',request.body)
+    console.log('/test post success')
+
+    //response.end(需要返回的数据)
+    response.end('xixi')
+})
+
+//2.将服务器应用实例挂载到某个端口上,并监听
+app.listen(3000,function(error){
+    if(error){
+        console.log(error)
+    }else{
+        console.log('服务器成功启动于:http://localhost:3000')
+    }
+})
