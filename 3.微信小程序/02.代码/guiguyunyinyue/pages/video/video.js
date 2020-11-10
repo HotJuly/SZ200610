f2073415510da12dc1afbce374a84954d618e4ea
HotJuly <949588380@qq.com>
2020-11-10 16:53:34 +0800

    36.视频模块(导航条动态效果)

--- 3.微信小程序/02.代码/guiguyunyinyue/pages/video/video.js
+++ 3.微信小程序/02.代码/guiguyunyinyue/pages/video/video.js
@@ -1,17 +1,39 @@
 // pages/video/video.js
+import ajax from '../../utils/ajax.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    navList:[],
+    currentId: 60100
+  },
 
+  changeId(event){
+    // console.log(event.currentTarget)
+    // id属性值数据类型是字符串,data-id属性值数据类型是原有类型
+    let id = event.currentTarget.id;
+    // console.log(id)
+    this.setData({
+      currentId:id*1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad:async function (options) {
+    let result = await ajax('/video/group/list');
+    let navList = result.data.slice(0,14);
+
+    //slice(开始下标,结束下标)->  左闭右包(包括开始的下标元素,不包括结束的)  ->不会对旧数组产生影响,会产生新数组
+    //splice(开始下标,切割数量,替换数据)  ->会对旧数组产生影响
+    // console.log(navList.splice(0, 14,"123"), navList);
+
+    this.setData({
+      navList
+    })
 
   },
 
