f95f8e984e47747a5916491aa4550eefce1b85cf
HotJuly <949588380@qq.com>
2020-11-11 14:49:47 +0800

    41.视频模块(常见功能)

--- 3.微信小程序/02.代码/guiguyunyinyue/pages/video/video.js
+++ 3.微信小程序/02.代码/guiguyunyinyue/pages/video/video.js
@@ -8,37 +8,69 @@ Page({
   data: {
     navList:[],
     currentId: null,
-    videoList:[]
+    videoList:[],
+    trigger:false
   },
 
+  //用于响应用户点击nav操作
   async changeId(event){
     // console.log(event.currentTarget)
     // id属性值数据类型是字符串,data-id属性值数据类型是原有类型
-    let id = event.currentTarget.id;
+    // let id = event.currentTarget.id;
+    let id = event.currentTarget.dataset.id;
     // console.log(id)
     this.setData({
-      currentId:id*1
+      currentId:id*1,
+      videoList:[]
     })
     wx.showLoading({
       title: '加载中,请稍等...'
     })
     await this.getVideoList();
-    console.log(2)
+    // console.log(2)
     wx.hideLoading();
   },
 
+  //用于请求视频列表数据
   async getVideoList(){
     //请求视频列表数据
     let videoListData = await ajax('/video/group', {
       id: this.data.currentId
     });
 
-    console.log(1)
+    // console.log(1)
 
     let videoList = videoListData.datas.map((item) => {
       return item.data;
     });
 
+    this.setData({
+      videoList
+    })
+  },
+
+  //用于下拉刷新videoList区域
+  async handlePullDown(){
+    // console.log('handlePullDown')
+    wx.showLoading({
+      title:"加载中,请稍等..."
+    });
+    await this.getVideoList();
+
+    //更新trigger状态,隐藏下拉的C3动画
+    this.setData({
+      trigger:false
+    })
+    wx.hideLoading();
+  },
+
+  //用于上拉加载更多数据
+  handleScrollToLower(){
+    // console.log('handleScrollToLower')
+    //发请求
+    let data = this.data.videoList.slice(0,2);
+    let {videoList} = this.data;
+    videoList=videoList.concat(data);
     this.setData({
       videoList
     })
@@ -97,14 +129,42 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    /*
+      下拉刷新
+        需求:当用户下拉页面或者某块区域,刷新当前页面数据(重新请求)
+        1.下拉页面进行刷新,触发onPullDownRefresh事件回调,发送请求获取最新数据
+          1)开启下拉动作  ->  app.json->window->enablePullDownRefresh:true
+          2)监听用户下拉操作  ->  触发onPullDownRefresh事件回调
+          3)发送请求获取最新数据  ->  使用封装好的函数getVideoList
 
+        2.下拉scroll-view组件进行刷新,触发bindrefresherrefresh事件回调,发送请求获取最新数据
+          1)开启下拉动作  ->  refresher-enabled
+          2)监听用户下拉操作  ->  触发bindrefresherrefresh事件回调
+          3)发送请求获取最新数据  ->  使用封装好的函数getVideoList
+
+          问题:数据已经回来,但是刷新的C3动画没有消失
+          需求:当数据回来之后,隐藏C3动画
+    */
+    console.log('onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    /*
+      上拉加载更多
+      需求:当用户上拉触底之后,请求更多数据,并且保留旧数据
+      拆解需求:
+        1)当用户上拉触底之后
+          页面触底  ->事件回调onReachBottom会被触发
+          scroll-view触底 ->事件回调scrolltolower
+        2)请求更多数据
+          发请求  ->  目前没有接口  ->  模拟数据
+        3)保留旧数据
+          合并旧数据的数组和新数据的数组
+     */
+    console.log('onReachBottom')
   },
 
   /**
