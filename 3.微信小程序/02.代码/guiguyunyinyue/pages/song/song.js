e40965faf76d8603bff36098fe715440f1deaf83
HotJuly <949588380@qq.com>
2020-11-13 11:20:47 +0800

    46.播放歌曲模块(重点:音乐播放)

--- 3.微信小程序/02.代码/guiguyunyinyue/pages/song/song.js
+++ 3.微信小程序/02.代码/guiguyunyinyue/pages/song/song.js
@@ -6,7 +6,64 @@ Page({
    * 页面的初始数据
    */
   data: {
+    songId:"",
+    songObj:{},
+    musicUrl:"",
+    isplaying:false
+  },
 
+  //响应用户点击播放按钮的操作
+  async handlePlay(){
+    // console.log('handlePlay')
+    /*
+      1.请求音频地址
+        this.getAudioUrl()
+      2.播放音频
+        wx.getBackgroundAudioManager()->得到BackgroundAudioManager(全局唯一的背景音频管理器实例) ->给实例对象添加src属性,就能自动播放音频
+        注意:如果想要实现进入后台之后还能播放歌曲,需要在app.json中配置
+      3.让页面进入播放状态
+
+      出现问题:无法暂停音频
+        如果当前页面正处于播放状态,再次点击播放按钮,应该暂停音频播放和页面动画
+        如果当前页面正处于暂停状态,再次点击播放按钮,应该开始音频播放和页面动画
+    */
+
+    let backgroundAudioManager = wx.getBackgroundAudioManager();
+    if (this.data.isplaying){
+      //isplaying为true就代表页面处于播放
+      backgroundAudioManager.pause();
+      this.setData({
+        isplaying: false
+      })
+    } else {
+      //isplaying为false就代表页面处于暂停
+
+      // 1.请求音频地址
+      await this.getAudioUrl();
+
+      // 2.播放音频
+      backgroundAudioManager.src = this.data.musicUrl;
+      backgroundAudioManager.title = this.data.songObj.name;
+
+      //3.让页面进入播放状态->通过状态控制页面是否要增加isplaying类名,从而控制页面的动画
+      this.setData({
+        isplaying: true
+      })
+    }
+    
+  },
+
+  //专门用于请求音频地址
+  async getAudioUrl(){
+    //发送请求,请求音频链接
+    let audioDetailInfo = await ajax('/song/url', {
+      id:this.data.songId
+    });
+    let { url } = audioDetailInfo.data[0];
+    this.setData({
+      musicUrl: url
+    })
+    // console.log('url', url)
   },
 
   /**
@@ -20,8 +77,10 @@ Page({
         2)获取歌曲id
           通过路由query传参,传递给onLoad的形参options
      */
-    console.log('options',options)
+    // console.log('options',options)
     let {id} = options;
+
+    //请求歌曲详细信息(不包括音频链接)
     let songDetailInfo = await ajax('/song/detail',{
       ids:id
     });
@@ -34,6 +93,7 @@ Page({
     wx.setNavigationBarTitle({
       title: this.data.songObj.name
     })
+
   },
 
   /**
