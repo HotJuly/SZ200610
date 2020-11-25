e5c41e3241ab63f4e3fd970c8046cdbf1ac7c9e8
HotJuly <949588380@qq.com>
2020-11-25 11:46:56 +0800

    5.iconfont图标使用

--- 4.uniapp/01.代码/uniapp-init/pages/index/index.vue
+++ 4.uniapp/01.代码/uniapp-init/pages/index/index.vue
@@ -3,6 +3,7 @@
 		<view class="header">
 			<image class="logo" src="../../static/images/logo.png" mode=""></image>
 			<view class="search">
+				<i class="iconfont icon-sousuo"></i>
 				<input type="text" placeholder="搜索商品" placeholder-class="placeholder" value="" />
 			</view>
 			<button class="username">七月</button>
@@ -40,12 +41,20 @@
 				flex-shrink 0
 				margin 0 20upx
 			.search
+				position relative
 				width 100%
 				background #ededed
+				padding-left 60upx
+				.iconfont
+					position absolute
+					left 20upx
+					top 50%
+					transform translateY(-50%)
 				.placeholder
 					font-size 24upx
 					color #333
 					text-align center
+					text-indent -40upx
 			.username
 				width 144upx
 				height 60upx
--- 4.uniapp/01.代码/uniapp-init/unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ 4.uniapp/01.代码/uniapp-init/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -145,6 +145,7 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
 var _default =
 {
   data: function data() {
